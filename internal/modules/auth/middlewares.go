da5eae0ba80ca8c5dbc32066c11a87e1b52c4361
Nikolay Grinko <ngrink.io@gmail.com>
2024-04-16 11:45:44 +0300

    add redirect to login for non-auth users

--- internal/modules/auth/middlewares.go
+++ internal/modules/auth/middlewares.go
@@ -37,6 +37,32 @@ func Authorized(next http.Handler) http.Handler {
 	})
 }
 
+func AuthorizedRedirect(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookieToken, err := r.Cookie("token")
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			return
+		}
+
+		token, err := validateJwtToken(cookieToken.Value)
+		if err != nil || !token.Valid {
+			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		userId, ok := claims["user_id"].(float64)
+		if !ok {
+			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), UserIdKey, uint(userId))
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
 func GetUserIdFromContext(r *http.Request) uint {
 	userId, ok := r.Context().Value(UserIdKey).(uint)
 	if !ok {
